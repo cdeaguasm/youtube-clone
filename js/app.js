5ec9c6ee8efb22b5bd29ebd92933d7f06f31ece3
carlos de aguas manga <cdeaguasm@gmail.com>
2020-04-29 19:05:59 -0500

    Add media queries css to responsy design

--- js/app.js
+++ js/app.js
@@ -4,4 +4,19 @@ document.querySelector("#menu-button").addEventListener("click", () => {
     container.classList.toggle("active");
 });
 
+function verifyResizeWindows() {
+    if(window.innerWidth <= 768) {
+        container.classList.remove('active');
+    }
+    else {
+        container.classList.add('active');
+    }
+}
+
+verifyResizeWindows();
+
+window.addEventListener('resize', () => {
+    verifyResizeWindows();
+});
+
 
